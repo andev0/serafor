291572499a265bd34c0ecfc533a5b56d761917d9
andev0 <fejalex@gmail.com>
2021-12-03 02:14:21 +0200

    Uploaded project to GitHub

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: serafor REGEX_QUERY");
+    
+    return;
+}
+
+string entryPoint = Directory.GetCurrentDirectory();
+string searchQuery = searchQuery = args[0];
+
+SearchAndPrintFiles(entryPoint, searchQuery);
+
+static void SearchAndPrintFiles(string directory, string searchQuery)
+{
+    Regex regex = new Regex(searchQuery, RegexOptions.Compiled);
+
+    try
+    {
+        foreach (string filename in Directory.EnumerateFiles(directory))
+        {
+            if (regex.IsMatch(Path.GetFileName(filename)))
+            {
+                Console.WriteLine(filename);
+            }
+        }
+
+        Parallel.ForEach(Directory.EnumerateDirectories(directory), (subDirectory) => 
+            SearchAndPrintFiles(subDirectory, searchQuery)
+        );
+    }
+    catch (UnauthorizedAccessException) { }
+    catch (DirectoryNotFoundException) { }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# serafor
+# Serafor
 Multithreaded C# CLI util for searching files using regex.
+
+<hr>
+
+Usage: 
+```
+serafor REGEX_QUERY
+```
+
+Usage example:
+```
+cd ~/Projects
+serafor .cpp
+```
