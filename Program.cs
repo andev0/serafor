6e0a19538ab0f29710f20c1a6bacdb32b172eff8
andev0 <fejalex@gmail.com>
2021-12-03 15:17:29 +0200

    Created ability for searching files or directories only
    - Created --help argument.
    - Created ability for searching files or directories only.
    - Created ability to search hidden files.

--- Program.cs
+++ Program.cs
@@ -3,39 +3,102 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Text;
+using System.Linq;
 
-Console.OutputEncoding = Encoding.UTF8;
-
-if (args.Length == 0)
+internal static class Program
 {
-    Console.WriteLine("Usage: serafor REGEX_QUERY");
-    
-    return;
-}
-
-string entryPoint = Directory.GetCurrentDirectory();
-string searchQuery = searchQuery = args[0];
-
-SearchAndPrintFiles(entryPoint, searchQuery);
-
-static void SearchAndPrintFiles(string directory, string searchQuery)
-{
-    Regex regex = new Regex(searchQuery, RegexOptions.Compiled);
-
-    try
+    [Flags]
+    private enum SearchModes
     {
-        foreach (string filename in Directory.EnumerateFiles(directory))
+        Files = 0b01,
+        Directories = 0b10
+    }
+    
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        if (args.Length == 0 || args.Contains("--help"))
         {
-            if (regex.IsMatch(Path.GetFileName(filename)))
-            {
-                Console.WriteLine(filename);
-            }
+            PrintHelp();
+            
+            return;
         }
 
-        Parallel.ForEach(Directory.EnumerateDirectories(directory), (subDirectory) => 
-            SearchAndPrintFiles(subDirectory, searchQuery)
-        );
+        string entryPoint = Directory.GetCurrentDirectory();
+
+        string regexQuery = args[0];
+        SearchModes searchMode = SearchModes.Files | SearchModes.Directories;
+        bool searchForHidden = false;
+
+        if (args.Contains("--files") || args.Contains("-f"))
+            searchMode = SearchModes.Files;
+
+        if (args.Contains("--directories") || args.Contains("-d"))
+            searchMode = SearchModes.Directories;
+
+        if (args.Contains("--hidden") || args.Contains("-h"))
+            searchForHidden = true;
+
+        SearchAndPrintFiles(entryPoint, regexQuery, searchMode, searchForHidden);
     }
-    catch (UnauthorizedAccessException) { }
-    catch (DirectoryNotFoundException) { }
+
+    private static void SearchAndPrintFiles(string directory, 
+                                            string regexQuery, 
+                                            SearchModes searchMode, 
+                                            bool includeHidden = false)
+    {
+        SearchAndPrintFiles(directory, new Regex(regexQuery, RegexOptions.Compiled), searchMode, includeHidden);
+
+        void SearchAndPrintFiles(string directory, 
+                                 Regex regexQuery, 
+                                 SearchModes searchMode, 
+                                 bool includeHidden = false)
+        {
+            try
+            {
+                if (searchMode.HasFlag(SearchModes.Files))
+                {
+                    foreach (string filename in Directory.EnumerateFiles(directory))
+                    {
+                        FileInfo fileInfo = new FileInfo(filename);
+
+                        if ((!fileInfo.Attributes.HasFlag(FileAttributes.Hidden) || includeHidden) &&
+                            regexQuery.IsMatch(fileInfo.Name))
+                        {
+                            Console.WriteLine(filename);
+                        }
+                    }
+                }
+
+                Parallel.ForEach(Directory.EnumerateDirectories(directory), (subDirectory) => {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(subDirectory);
+
+                    if ((!directoryInfo.Attributes.HasFlag(FileAttributes.Hidden) || includeHidden))
+                    {
+                        if (searchMode.HasFlag(SearchModes.Directories) &&
+                            regexQuery.IsMatch(directoryInfo.Name))
+                        {
+                            Console.WriteLine(subDirectory);
+                        }
+
+                        SearchAndPrintFiles(subDirectory, regexQuery, searchMode, includeHidden);
+                    }
+                });
+            }
+            catch (UnauthorizedAccessException) { }
+            catch (DirectoryNotFoundException) { }
+        }
+    }
+
+    private static void PrintHelp()
+    {
+        Console.WriteLine("Usage: serafor REGEX-QUERY [OPTIONS]...");
+        Console.WriteLine("Search files or directories using regex query");
+        Console.WriteLine();
+        Console.WriteLine("Options:");
+        Console.WriteLine($"{"  -f, --files",       -25}Search for files only.");
+        Console.WriteLine($"{"  -d, --directories", -25}Search for directories only.");
+        Console.WriteLine($"{"  -h, --hidden", -25}Include hidden files and directories in the search.");
+    }
 }
